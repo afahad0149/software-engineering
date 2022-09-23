64323236d26382cd89d63b64b7db1f00ad173f69
quantium <alexcollsoutumuro@gmail.com>
2022-09-23 19:56:03 +0200

    finished dance.js

--- sub-class-dance-party-exercise/scripts/dance.js
+++ sub-class-dance-party-exercise/scripts/dance.js
@@ -3,6 +3,91 @@ $(() => {
 
   class Dancer {
 
+    constructor ( top, left ) {
+
+      if ( !top ) this.top = this.randomStagePosition();
+      else this.top = top;
+
+      if ( !left ) this.left = this.randomStagePosition();
+      else this.left = left;
+      
+      this.$node = $('<div class="dancer"></div>');
+
+      this.setPosition(this.top, this.left);
+
+    }
+
+    setPosition (top, left) {
+      this.$node.css({top, left});
+    }
+
+    tapDancer () {
+      return this.$node.addClass('blink');
+    }
+
+    rainbowDancer () {
+      return this.$node.addClass('rainbow');
+    }
+
+    randomStagePosition () {
+      return String(Math.random() * 100 | 0)+'%';
+    }
+
   }
 
+  $('#regular').click( function () {
+    const dancer = new Dancer();
+    $('#stage').append( dancer.$node );
+  });
+
+  $('#tapper').click( function () {
+    const dancer = new Dancer();
+    $('#stage').append( dancer.tapDancer() );
+  });
+
+  $('#rainbow').click( function () {
+    const dancer = new Dancer();
+    $('#stage').append( dancer.rainbowDancer() );
+  });
+
+
+
+
+  function letsDance (selector) {
+
+    const h = $(window).height();
+    const w = $(window).width();
+    
+    const nh = Math.floor(Math.random() * h);
+    const nw = Math.floor(Math.random() * w);
+    
+    $(selector).animate({ top: nh, left: nw }, 1000, function () { letsDance(selector); });
+    
+  }
+
+  $('#dance').click( function () {
+
+    const elements = $('.dancer').get();
+
+    elements.forEach( (element) => {
+      element.startPos = $(element).offset();
+      letsDance(element);
+    });
+
+  });
+
+  $('#rest').click( function () {
+
+    const elements = $('.dancer').get();
+
+    elements.forEach( (element) => {
+      $(element).stop();
+      $(element).css({top: element.startPos.top, left: element.startPos.left});
+    });
+
+  });
+
+
 });
+
+
